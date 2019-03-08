1798c8e8b0492382d719cb5ee3175e2db7a18747
Sarmad Aijaz <azaabudeen@gmail.com>
2019-03-08 23:30:22 +0530

    image upload

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,4 +1,5 @@
 import React, {useState, useContext} from 'react';
+import axios from 'axios';
 import {withStyles} from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
@@ -20,9 +21,25 @@ const CreatePin = ({classes}) => {
     setContent ('');
     dispatch ({type: 'DELETE_DRAFT'});
   };
-  const handleSubmit = e => {
+  const handleImageUpload = async () => {
+    try {
+      const data = new FormData ();
+      data.append ('file', image);
+      data.append ('upload_preset', 'geopins');
+      data.append ('cloud_name', 'dtmt9wiwn');
+      const response = await axios.post (
+        'https://api.cloudinary.com/v1_1/dtmt9wiwn/image/upload',
+        data
+      );
+      return response.data.url;
+    } catch (e) {
+      console.log (e);
+    }
+  };
+  const handleSubmit = async e => {
     e.preventDefault ();
-    console.log ({title, image, content});
+    const url = await handleImageUpload ();
+    console.log ({title, image, content, url});
   };
   return (
     <form className={classes.form}>

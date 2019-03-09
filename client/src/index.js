34951703c2e986c10b8029a63c098a4942b44233
Sarmad Aijaz <azaabudeen@gmail.com>
2019-03-10 00:42:39 +0530

    setting apooloclient

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
-import React, { useContext, useReducer } from 'react';
+import React, {useContext, useReducer} from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import App from './pages/App';
 import Splash from './pages/Splash';
@@ -8,28 +8,45 @@ import Context from './context';
 import reducer from './reducer';
 import ProtectedRoute from './ProtectedRoute';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import {ApolloProvider} from 'react-apollo';
+import {ApolloClient} from 'apollo-client';
+import {WebSocketLink} from 'apollo-link-ws';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 import * as serviceWorker from './serviceWorker';
 
-const Root = () => {
-  const initialState = useContext(Context);
-  const [state, dispatch] = useReducer(reducer, initialState);
+const wsLink = new WebSocketLink ({
+  uri: 'ws://localhost:4000/graphql',
+  options: {
+    reconnect: true,
+  },
+});
+const client = new ApolloClient ({
+  link: wsLink,
+  cache: InMemoryCache,
+});
 
-  console.log(state);
+const Root = () => {
+  const initialState = useContext (Context);
+  const [state, dispatch] = useReducer (reducer, initialState);
+
+  console.log (state);
   return (
     <Router>
-      <Context.Provider value={{ state, dispatch }}>
-        <Switch>
-          <ProtectedRoute exact path="/" component={App} />
-          <Route path="/login" component={Splash} />
-        </Switch>
-      </Context.Provider>
+      <ApolloProvider client={client}>
+        <Context.Provider value={{state, dispatch}}>
+          <Switch>
+            <ProtectedRoute exact path="/" component={App} />
+            <Route path="/login" component={Splash} />
+          </Switch>
+        </Context.Provider>
+      </ApolloProvider>
     </Router>
   );
 };
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render (<Root />, document.getElementById ('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister ();
--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,9 @@
-const {AuthenticationError} = require ('apollo-server');
+const {AuthenticationError, PubSub} = require ('apollo-server');
 const Pin = require ('./models/Pin');
-
+const pubSub = new PubSub ();
+const PIN_ADDED = 'PIN_ADDED';
+const PIN_UPDATED = 'PIN_UPDATED';
+const PIN_DELETED = 'PIN_DELETED';
 const authenticated = next => (root, args, context, info) => {
   if (!context.currentUser) {
     throw new AuthenticationError ('You must be logged in ');
@@ -25,6 +28,7 @@ module.exports = {
           author: context.currentUser._id,
         }).save ();
         const pinAdded = await Pin.populate (newPin, 'author');
+        pubSub.publish (PIN_ADDED, {pinAdded});
         return pinAdded;
       } catch (e) {
         console.log (e);
@@ -33,6 +37,8 @@ module.exports = {
     }),
     deletePin: authenticated (async (root, args, context) => {
       const pinDeleted = await Pin.findOneAndDelete ({_id: args.pinId}).exec ();
+      pubSub.publish (PIN_DELETED, {pinDeleted});
+
       return pinDeleted;
     }),
     createComment: authenticated (async (root, args, context) => {
@@ -44,7 +50,19 @@ module.exports = {
       )
         .populate ('author')
         .populate ('comments.author');
+      pubSub.publish (PIN_UPDATED, {pinUpdated});
       return pinUpdated;
     }),
+  },
+  Subscription: {
+    pinAdded: {
+      subscribe: () => pubSub.asyncIterator (PIN_ADDED),
+    },
+    pinDeleted: {
+      subscribe: () => pubSub.asyncIterator (PIN_DELETED),
+    },
+    pinUpdated: {
+      subscribe: () => pubSub.asyncIterator (PIN_UPDATED),
+    },
   },
 };

39f3a387590b05222952f1a51fce616ff46bf231
Sarmad Aijaz <azaabudeen@gmail.com>
2019-03-09 21:23:07 +0530

    pin content

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -4,17 +4,19 @@ import {Paper} from '@material-ui/core';
 import Context from '../context';
 import NoContent from './Pin/NoContent';
 import CreatePin from './Pin/CreatePin';
-
+import PinContent from './Pin/PinContent';
 const Blog = ({classes}) => {
   const {state} = useContext (Context);
-  const {draft} = state;
+  const {draft, currentPin} = state;
   let BlogContext;
-  if (!draft) {
+  if (!draft && !currentPin) {
     BlogContext = NoContent;
     // no content
-  } else if (draft) {
+  } else if (draft && !currentPin) {
     BlogContext = CreatePin;
     // create ping component
+  } else if (!draft && currentPin) {
+    BlogContext = PinContent;
   }
   return <Paper className={classes.root}><BlogContext /></Paper>;
 };

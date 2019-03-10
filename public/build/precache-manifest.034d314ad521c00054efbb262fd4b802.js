1249a29d9527a848fbb257f26f3366f561463e71
Sarmad Aijaz <azaabudeen@gmail.com>
2019-03-10 23:27:26 +0530

    reactbuild

---
+++ public/build/precache-manifest.034d314ad521c00054efbb262fd4b802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66d4c943d18f68c86041",
+    "url": "/static/js/main.66d4c943.chunk.js"
+  },
+  {
+    "revision": "3f6e624405e46ea63bb4",
+    "url": "/static/css/1.e887b803.chunk.css"
+  },
+  {
+    "revision": "3f6e624405e46ea63bb4",
+    "url": "/static/js/1.3f6e6244.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2fd0d682d251fe543c44936cdaf3c15b",
+    "url": "/index.html"
+  }
+];
